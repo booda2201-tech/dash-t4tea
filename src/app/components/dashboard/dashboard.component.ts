6b58e4ed249ecd1e12c15e127f15a4a97a6a1d82
booda2201-tech <booda2201@gmail.com>
2026-07-14 15:39:26 +0300

    to update

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
+import { Subscription, filter } from 'rxjs';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
@@ -10,12 +11,42 @@ import { AuthService } from '../../services/auth.service';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
-  constructor(private auth: AuthService) {}
+export class DashboardComponent implements OnInit, OnDestroy {
+  menuOpen = false;
+  private routerSub?: Subscription;
 
-  ngOnInit(): void {}
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.routerSub = this.router.events
+      .pipe(filter((e): e is NavigationEnd => e instanceof NavigationEnd))
+      .subscribe(() => this.closeMenu());
+  }
+
+  ngOnDestroy(): void {
+    this.routerSub?.unsubscribe();
+  }
+
+  @HostListener('window:resize')
+  onResize(): void {
+    if (window.innerWidth >= 992) {
+      this.menuOpen = false;
+    }
+  }
+
+  toggleMenu(): void {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu(): void {
+    this.menuOpen = false;
+  }
 
   logout(): void {
+    this.closeMenu();
     this.auth.logoutAndRedirect();
   }
 }
